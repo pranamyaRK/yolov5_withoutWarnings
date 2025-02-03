0f3ce7b7a8ff965eb460584dd286be09294fe3e3
Pranamya Rajashekhar <112478996+pranamyaRK@users.noreply.github.com>
2025-02-03 23:33:35 +0530

    Update val.py to suppress futureWarnings
    Signed-off-by: Pranamya Rajashekhar <112478996+pranamyaRK@users.noreply.github.com>

--- val.py
+++ val.py
@@ -30,6 +30,10 @@ import numpy as np
 import torch
 from tqdm import tqdm
 
+import warnings
+# Suppress FutureWarnings from PyTorch
+warnings.simplefilter(action='ignore', category=FutureWarning)
+
 FILE = Path(__file__).resolve()
 ROOT = FILE.parents[0]  # YOLOv5 root directory
 if str(ROOT) not in sys.path:
